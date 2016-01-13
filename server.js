04f7c24ab61e9013aaa5b8b60cd069823b14803b
Olof Astrand <olof.astrand@gmail.com>
2016-01-13 03:11:05 +0000

    Added first index.html test

---
+++ server.js
@@ -0,0 +1,33 @@
+/*
+ *  Node/Mongo server
+ *
+ *
+ */
+
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
+// Logging
+var morgan = require('morgan');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+	    extended: true
+	}));
+
+// Log all requests
+app.use(morgan("dev"));
+
+
+app.use('/',express.static(__dirname + '/public'));
+//app.use('/dist',express.static(__dirname + '../../../dist'));
+//app.use('/libs',express.static(__dirname + '../../../libs'));
+
+// Prevent, 304 (Not changed) for api
+//app.disable('etag');
+
+app.listen(10080);
